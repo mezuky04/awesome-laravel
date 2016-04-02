6ea0b0dcb5c1cb9a7778a3a6e7ae85c773a64372
mezuky04 <alexandru.bugarin@gmail.com>
2016-04-02 07:56:49 +0300

    Homepage extends base layout and use vue template

---
+++ resources/assets/js/components/Resources.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="row">
+
+        <div class="col-md-4 col-md-offset-4 text-center">
+            <h3 class="blue-grey-text">Choose a category</h3>
+            <select class="selectpicker" data-style="btn-ghost-inverse">
+                <option>Mustard</option>
+                <option>Ketchup</option>
+                <option>Relish</option>
+                <option>Relish</option>
+                <option>Relish</option>
+            </select>
+        </div>
+
+    </div>
+
+    <div class="row">
+        <h3 class="blue-grey-text">Last resources added</h3>
+        <div class="list-group">
+            <a href="#" class="list-group-item"><strong>First item</strong> - short resource description goes here</a>
+            <a href="#" class="list-group-item">Second item</a>
+            <a href="#" class="list-group-item">Third item</a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
