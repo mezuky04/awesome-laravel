e1241e5b3a8ab0c4739eff384b6f9a8a4e58dc08
mezuky04 <alexandru.bugarin@gmail.com>
2016-04-02 14:17:08 +0300

    Add categories

--- database/seeds/ResourceCategoriesTableSeeder.php
+++ database/seeds/ResourceCategoriesTableSeeder.php
@@ -12,9 +12,25 @@ class ResourceCategoriesTableSeeder extends Seeder {
     public function run() {
 
         $categories = [
-            ['name' => 'Articles'],
-            ['name' => 'Tutorials'],
-            ['name' => 'Essentials']
+            ['name' => 'Essentials'],
+            ['name' => 'Code Snippets'],
+            ['name' => 'Packages'],
+            ['name' => 'Popular Packages'],
+            ['name' => 'Development Setup'],
+            ['name' => 'Application Hosting'],
+            ['name' => 'Application Deployment'],
+            ['name' => 'Articles, Tutorials, Blogs etc.'],
+            ['name' => 'Videos'],
+            ['name' => 'Conferences'],
+            ['name' => 'Books'],
+            ['name' => 'Starter Projects'],
+            ['name' => 'Codebase for Reference'],
+            ['name' => 'Newsletters'],
+            ['name' => 'Podcasts'],
+            ['name' => 'Community'],
+            ['name' => 'Jobs'],
+            ['name' => 'Hosted Development Tools'],
+            ['name' => 'Miscellaneous'],
         ];
 
         foreach ($categories as $category) {
